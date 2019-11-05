b06d0ea7b07b11e5aea05be21ee642439cd3f954
Michelle Kim <x1michelleskim@gmail.com>
2019-11-04 22:15:44 -0800

    Write a valid moves class method to test the 8 possible moves for a knight

--- KnightsTraversal/knights_traversal_1.rb
+++ KnightsTraversal/knights_traversal_1.rb
@@ -1,6 +1,21 @@
 class KnightPathFinder
+
+    MOVES = [
+        [1,2],
+        [-1, 2],
+        [-1, -2],
+        [1, -2],
+        [2, 1],
+        [-2, -1],
+        [-2, 1],
+        [2, -1]
+    ]
+
     def initialize(start_pos)
         @start_pos = start_pos
     end
 
+    def self.valid_moves(pos)
+        
+    end
 end
