cace74345452eb5da0cddfb9f28ef3208f5a534b
Michelle Kim <x1michelleskim@gmail.com>
2019-11-04 22:21:12 -0800

    Finish vaRefactor valid moves to have the considered_positions instance variable check in the new_move_positions method

--- KnightsTraversal/knights_traversal_1.rb
+++ KnightsTraversal/knights_traversal_1.rb
@@ -13,9 +13,22 @@ class KnightPathFinder
 
     def initialize(start_pos)
         @start_pos = start_pos
+
+        @considered_positions = [@start_pos]
     end
 
     def self.valid_moves(pos)
+        valid = []
+        MOVES.each do |move|
+            if !@considered_positions.include?(move)
+                valid << move
+                @considered_positions << move
+            end
+        end
+        valid
+    end
+
+    def new_move_position(pos)
         
     end
 end
