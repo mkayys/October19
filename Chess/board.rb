09584918476eb2ff628a475a55c6946ad94f2187
Michelle Kim <x1michelleskim@gmail.com>
2019-11-05 13:55:09 -0800

    Create board class to create a grid and populate the board

---
+++ Chess/board.rb
@@ -0,0 +1,16 @@
+class Board
+
+    def initialize
+        @grid = Array.new(8) { Array.new(8, nil) }
+    end
+    
+    def place_pieces
+
+        @grid.each_with_index do |row, i|
+            row.each_with_index do |spot, j|
+                
+            end
+        end
+    end
+
+end
