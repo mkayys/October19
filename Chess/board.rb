06cdb1f9db369f58fc702738be38dff9d8b2b162
Michelle Kim <x1michelleskim@gmail.com>
2019-11-05 14:57:13 -0800

    Finish moving one piece around the board

--- Chess/board.rb
+++ Chess/board.rb
@@ -21,10 +21,25 @@ class Board
         @grid.each_with_index do |row, i|
             row.each_with_index do |pos, j|
                 if (i == 0) || (i == 1) || (i == 6) || (i == 7)
+                    # @grid[i][j] = Piece.new
                     self[[i, j]] = Piece.new
                 end
             end
         end
+    end
+
+    def move_piece(start_pos, end_pos)
+        if self[start_pos].nil?
+            raise "THERE IS NO PIECCCEE"
+        end
+        # piece can't move to end_pos
+        # 1. if another piece of your color is there
+        # 2. if it's not on the grid
+        # 3. the piece can't move that way
+        moving_piece = self[start_pos]
+        self[end_pos] = moving_piece
+        self[start_pos] = nil
+        
     end
 
 end
