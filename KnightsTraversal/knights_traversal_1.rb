7752dbd017932689b0076a0b1a67cd01f21fa5ff
Michelle Kim <x1michelleskim@gmail.com>
2019-11-04 22:26:08 -0800

    Finish valid moves and new move position methods

--- KnightsTraversal/knights_traversal_1.rb
+++ KnightsTraversal/knights_traversal_1.rb
@@ -19,16 +19,25 @@ class KnightPathFinder
 
     def self.valid_moves(pos)
         valid = []
+        x, y = pos
         MOVES.each do |move|
-            if !@considered_positions.include?(move)
-                valid << move
-                @considered_positions << move
+            if !(x + move[0] > 8 || x + move[0] < 0) && !(y + move[1] < 0 || y + move[1] > 8)
+                valid << [x + move[0], y + move[1]]
             end
         end
         valid
     end
 
     def new_move_position(pos)
-        
+        moves = KnightPathFinder.valid_moves(pos)
+        new_moves = []
+        moves.each do |move|
+            if !@considered_positions.include?(move)
+                new_moves << move
+                @considered_positions << move
+            end
+        end
+
+        new_moves
     end
 end
