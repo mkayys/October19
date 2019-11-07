accbb943d92be64048fd90fb450338a17fdd51f3
Michelle Kim <x1michelleskim@gmail.com>
2019-11-07 10:56:23 -0800

    Refactor spec to use subject

--- RspecLec/spec/sloth_spec.rb
+++ RspecLec/spec/sloth_spec.rb
@@ -2,9 +2,14 @@ require 'sloth' # automatically searches lib folder for this file
 # require 'rspec'
 
 describe Sloth do
+
+    subject(:sloth) { Sloth.new('Gerald') }
+    # to not be redundant for each initialize case
+    # be careful of using to be (checking if same object id) vs to eq
+
     describe '#initialize' do
         it "assigns a name successfully" do
-            sloth = Sloth.new('Gerald')
+            # sloth = Sloth.new('Gerald')
             expect(sloth.name).to eq('Gerald')
         end
 
@@ -14,7 +19,7 @@ describe Sloth do
         end
 
         it "assigns an empty drinks hash" do
-            sloth = Sloth.new('Gerald')
+            # sloth = Sloth.new('Gerald')
             expect(sloth.drinks).to eq({})
         end
     end
