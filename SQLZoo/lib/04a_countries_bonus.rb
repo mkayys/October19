d69f2cc64e144817da7ac0d5eb537bd5881b32de
Michelle Kim <x1michelleskim@gmail.com>
2019-11-12 10:31:16 -0800

    Finish aggregate sql problems

--- SQLZoo/lib/04a_countries_bonus.rb
+++ SQLZoo/lib/04a_countries_bonus.rb
@@ -17,6 +17,9 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHERE
   SQL
 end
 
