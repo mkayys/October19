3058aeb5cc4f95afe8f0b999aa9c0a1465eeee0c
Michelle Kim <x1michelleskim@gmail.com>
2019-11-25 11:21:10 -0800

    Create session actions and render new form for signing in

--- 99Cats2/app/controllers/users_controller.rb
+++ 99Cats2/app/controllers/users_controller.rb
@@ -8,11 +8,21 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            
+
         else
+            render :new
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+        
+        if @user
+            render :show
+        else
+            # redirect_to 
+        end
+    end
     private
     def user_params
         params.require(:user).permit(:username, :password)
