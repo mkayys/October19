109d24d5712f90a84163b7485c2a66d3cd070ac2
Michelle Kim <x1michelleskim@gmail.com>
2019-11-25 11:23:40 -0800

    Refactor controller actions

--- 99Cats2/app/controllers/sessions_controller.rb
+++ 99Cats2/app/controllers/sessions_controller.rb
@@ -5,6 +5,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by_credentials(
+            params[:user][:username]
+            params[:user][:password]
+        )
+
+        if @user
+            redirect_to cats_url
+        else
+        end
 
     end
 
