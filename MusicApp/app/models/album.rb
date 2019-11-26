0b3d287ca4fbbd80e811eea9e2cc90f8930d6520
Michelle Kim <x1michelleskim@gmail.com>
2019-11-26 11:10:05 -0800

    Write an association for the album to belong to a band

--- MusicApp/app/models/album.rb
+++ MusicApp/app/models/album.rb
@@ -1,2 +1,8 @@
 class Album < ApplicationRecord
+    validates :title, :year, :band_id, :live, presence: true
+    validates :title, uniqueness: true
+    vaildates :band_id, uniqueness: true
+
+    belongs_to :band
+
 end
