f795e2d77b7393cd986747bfea31dc2f8f7fb477
Michelle Kim <x1michelleskim@gmail.com>
2019-11-26 09:51:59 -0800

    Create views for signing up and signing in a user

---
+++ MusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username]
+        )
+    end
+
+    def destroy
+    end
+
+end
