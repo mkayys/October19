bb5d33f84ba6314461fd3c321d0d2a1666438109
Michelle Kim <x1michelleskim@gmail.com>
2019-12-02 09:43:56 -0800

    Create forms for signing up and signing in and add table for subs

--- RedditClone/app/controllers/sessions_controller.rb
+++ RedditClone/app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(username, password)
+        
+        if @user
+            login!(@user)
+            # redirect_to
+        else
+            flash.now[:errors] = ["Invalid credentials"]
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+        # redirect_to 
+    end
 end
