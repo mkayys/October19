005cd6237627219bd7d5d44eefae8094c1b979ed
Michelle Kim <x1michelleskim@gmail.com>
2019-12-02 12:05:51 -0800

    Write validtaions for Post model and associations

--- RedditClone/app/models/sub.rb
+++ RedditClone/app/models/sub.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :bigint           not null, primary key
+#  title        :string           not null
+#  description  :string
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Sub < ApplicationRecord
 
     validates :title, presence: true
@@ -6,5 +18,5 @@ class Sub < ApplicationRecord
         foreign_key: :moderator_id,
         class_name: :User
 
-    
+    has_many :posts
 end
--- RedditClone/app/models/user.rb
+++ RedditClone/app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
     before_validation :ensure_session_token
 
     has_many :subs
+    has_many :posts
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
