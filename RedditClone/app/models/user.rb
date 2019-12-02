dc0ffa8a90ba967abc77d4d5cf7074ac3b3e6692
Michelle Kim <x1michelleskim@gmail.com>
2019-12-01 19:06:26 -0800

    Add model-level validations for users information

--- RedditClone/app/models/user.rb
+++ RedditClone/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    attr_reader :password
+
+    before_validation :ensure_session_token
+    
 end
