92a612eeae0ba8f5e5cb24a92c8e0fbd14a534e4
Michelle Kim <x1michelleskim@gmail.com>
2019-12-09 12:24:19 -0800

    Begin play in Game class

--- FlappyBird/src/bird.js
+++ FlappyBird/src/bird.js
@@ -1,23 +1,35 @@
+const CONSTANTS = {
+    GRAVITY: 0.8,
+    FLAP_SPEED: -8,
+    TERMINAL_VEL: 12,
+    BIRD_WIDTH: 40,
+    BIRD_HEIGHT: 30
+};
+
 export default class Bird {
     constructor(dimensions) {
         this.velocity = 0;
         this.dimensions = dimensions;
         this.pos = { width: Math.floor(this.dimensions.width / 3), height: Math.floor(this.dimensions.height / 2) };
-        this.y = this.pos.height;
-        this.x = this.pos.width;
     }
 
     animate(ctx) {
+        this.move();
         this.drawBird(ctx);
     }
 
     drawBird(ctx) {
         ctx.fillStyle = "yellow";
-        ctx.fillRect(this.pos.width, this.pos.height, 40, 30);
+        ctx.fillRect(this.pos.width, this.pos.height, CONSTANTS.BIRD_WIDTH, CONSTANTS.BIRD_HEIGHT);
     }
 
     move() {
-        this.velocity += this.y
-        this.velocity += 0.5 // for gravity
+        this.pos.height += this.velocity;
+        this.velocity += CONSTANTS.GRAVITY; // for gravity
+    }
+
+    flap() {
+        this.velocity = CONSTANTS.FLAP_SPEED;
+
     }
 }
