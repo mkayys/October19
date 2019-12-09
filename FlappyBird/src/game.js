679732f2563b76b92f480f921fef2e60c1b13855
Michelle Kim <x1michelleskim@gmail.com>
2019-12-09 12:34:46 -0800

    Bird dropping out of frame on refresh

--- FlappyBird/src/game.js
+++ FlappyBird/src/game.js
@@ -5,17 +5,32 @@ export default class FlappyBird {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+    this.animate = this.animate.bind(this);
     this.restart();
+    this.play();
   }
 
   animate() {
     this.level.animate(this.ctx);
     this.bird.animate(this.ctx);
+    if (this.running) {
+      requestAnimationFrame(this.animate);
+    }
   }
 
   restart() {
+    this.running = false;
     this.level = new Level(this.dimensions);
     this.bird = new Bird(this.dimensions);
     this.animate();
+  }
+  
+  play() {
+    this.running = true;
+    this.animate();
+  }
+
+  click() {
+    
   }
 }
