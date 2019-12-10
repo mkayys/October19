0d6f3626f09dc0964a048807b48bd2751013b824
Michelle Kim <x1michelleskim@gmail.com>
2019-12-10 09:41:33 -0800

    Inheritance for Asteroid and MovingObject

--- Asteroids/src/asteroid.js
+++ Asteroids/src/asteroid.js
@@ -10,10 +10,14 @@ function Asteroid(options) {
     
     options = options || {};
     options.pos = options.pos;
-    options.vel = options.vel;
+    options.vel = options.vel || Util.randomVec(10);
     options.radius = DEFAULTS.RADIUS;
     options.color = DEFAULTS.COLOR;
 
     MovingObject.call(this, options)
 
 }
+
+Util.inherits(Asteroid, MovingObject);
+
+module.exports = Asteroid;
