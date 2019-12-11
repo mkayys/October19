f6a2226c660587c9374843a8921901c22aab3e80
Michelle Kim <x1michelleskim@gmail.com>
2019-12-11 11:07:30 -0800

    Add CSS and bind click event handler to add the currentplayer mark

--- TicTacToe/dist/main.js
+++ TicTacToe/dist/main.js
@@ -137,7 +137,7 @@ eval("\nconst MoveError = function (msg) { this.msg = msg; };\n\n// MoveError re
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("class View {\n  constructor(game, $el) {\n    this.game = game;\n    this.$el = $el;\n  }\n\n  bindEvents() {\n    \n  }\n\n  makeMove($square) {\n\n  }\n\n  setupBoard() {\n\n  }\n}\n\nmodule.exports = View;\n\n\n//# sourceURL=webpack:///./src/ttt-view.js?");
+eval("class View {\n  constructor(game, $el) {\n    this.game = game;\n    this.$el = $el;\n    this.setupBoard();\n    this.bindEvents();\n  }\n\n  bindEvents() {\n    $('.box').each((i, li) => {\n      let $li = $(li);\n      $li.on('click', () => {\n        // console.log($li);\n        let pos = $li.data('pos');\n        // console.log(pos);\n        this.game.playMove(pos);\n        this.makeMove($li);\n      })\n    })\n  }\n\n  makeMove($square) {\n    $square.html(`${this.game.currentPlayer}`);\n  }\n\n  setupBoard() {\n    const $grid = $('<ul></ul>');\n    $grid.addClass('grid')\n    this.$el.append($grid);\n    for (let i = 0; i < 3; i++) {\n      for (let j = 0; j < 3; j++) {\n        let $box = $('<li></li>');\n        $box.addClass('box');\n        $box.data('pos', [i, j]);\n        $grid.append($box);\n      }\n    }\n\n  }\n}\n\nmodule.exports = View;\n\n\n//# sourceURL=webpack:///./src/ttt-view.js?");
 
 /***/ })
 
--- TicTacToe/src/ttt-view.js
+++ TicTacToe/src/ttt-view.js
@@ -2,17 +2,39 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
   }
 
   bindEvents() {
-    
+    $('.box').each((i, li) => {
+      let $li = $(li);
+      $li.on('click', () => {
+        // console.log($li);
+        let pos = $li.data('pos');
+        // console.log(pos);
+        this.game.playMove(pos);
+        this.makeMove($li);
+      })
+    })
   }
 
   makeMove($square) {
-
+    $square.html(`${this.game.currentPlayer}`);
   }
 
   setupBoard() {
+    const $grid = $('<ul></ul>');
+    $grid.addClass('grid')
+    this.$el.append($grid);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $box = $('<li></li>');
+        $box.addClass('box');
+        $box.data('pos', [i, j]);
+        $grid.append($box);
+      }
+    }
 
   }
 }
