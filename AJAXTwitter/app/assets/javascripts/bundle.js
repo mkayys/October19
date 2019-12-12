ffbf2bb6aa08e372cd99ceaefdcd23c868db24b5
Michelle Kim <x1michelleskim@gmail.com>
2019-12-12 11:51:45 -0800

    Finish toggle button and work on refactoring ajax calls into its own file

--- AJAXTwitter/app/assets/javascripts/bundle.js
+++ AJAXTwitter/app/assets/javascripts/bundle.js
@@ -93,7 +93,63 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: Unexpected token (10:4)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|     render() {\n|         if (this.followState === \"followed\")\n>     }\n| }\n| ");
+class FollowToggle {
+    constructor(el) {
+        this.$el = $(el);
+        this.userId = this.$el.data('user-id');
+        this.followState = this.$el.data('initial-follow-state');
+        this.toggleState = this.toggleState.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.render = this.render.bind(this);
+        this.bindEvent();
+        this.render();
+    }
+
+    bindEvent() {
+        this.$el.on('click', this.handleClick);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        let method;
+        if (this.followState === "unfollowed") {
+            method = 'post';
+        } else {
+            // console.log('i should be here')
+            method = 'delete'
+        }
+
+        $.ajax({
+            method: method,
+            url: `/users/${this.userId}/follow`,
+            dataType: 'JSON'
+        }).then(res => {
+            
+            this.toggleState();
+            // console.log(this.followState)
+            this.render();
+        });
+    }
+
+    toggleState() {
+        
+        if (this.followState === "followed") {
+            this.followState = "unfollowed";
+        } else {
+            this.followState = "followed";
+        }
+    }
+
+    render() {
+        if (this.followState === "followed") {
+            this.$el.html('Unfollow!');
+        } else {
+            this.$el.html('Follow!')
+        }
+    }
+}
+
+module.exports = FollowToggle;
 
 /***/ }),
 
--- AJAXTwitter/frontend/follow_toggle.js
+++ AJAXTwitter/frontend/follow_toggle.js
@@ -2,11 +2,55 @@ class FollowToggle {
     constructor(el) {
         this.$el = $(el);
         this.userId = this.$el.data('user-id');
-        this.FollowState = this.$el.data('initial-follow-state');
+        this.followState = this.$el.data('initial-follow-state');
+        this.toggleState = this.toggleState.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.render = this.render.bind(this);
+        this.bindEvent();
+        this.render();
+    }
+
+    bindEvent() {
+        this.$el.on('click', this.handleClick);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        let method;
+        if (this.followState === "unfollowed") {
+            method = 'post';
+        } else {
+            // console.log('i should be here')
+            method = 'delete'
+        }
+
+        $.ajax({
+            method: method,
+            url: `/users/${this.userId}/follow`,
+            dataType: 'JSON'
+        }).then(res => {
+            
+            this.toggleState();
+            // console.log(this.followState)
+            this.render();
+        });
+    }
+
+    toggleState() {
+        
+        if (this.followState === "followed") {
+            this.followState = "unfollowed";
+        } else {
+            this.followState = "followed";
+        }
     }
 
     render() {
-        if (this.followState === "followed")
+        if (this.followState === "followed") {
+            this.$el.html('Unfollow!');
+        } else {
+            this.$el.html('Follow!')
+        }
     }
 }
 
