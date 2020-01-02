3cefdb79bf51bd9231159b9a1177d73ee7bc3547
Michelle Kim <x1michelleskim@gmail.com>
2020-01-02 10:46:21 -0800

    Test controller actions for login, signup, signin sucessfully

--- BenchBnB/app/models/user.rb
+++ BenchBnB/app/models/user.rb
@@ -20,14 +20,14 @@ class User < ApplicationRecord
     end
 
     def is_password?(password)
-        BCyrpt::Password.new(self.password_digest).is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
     def ensure_session_token
         self.session_token ||= SecureRandom.urlsafe_base64
     end
 
-    def reset_session_token
+    def reset_session_token!
         self.session_token = SecureRandom.urlsafe_base64
         self.save!
         self.session_token
