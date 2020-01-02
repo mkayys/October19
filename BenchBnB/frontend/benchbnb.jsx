ea66c25e52ed944db7300581303ec15ad60593e3
Michelle Kim <x1michelleskim@gmail.com>
2020-01-02 11:07:55 -0800

    Require jquery and test AJAX calls

--- BenchBnB/frontend/benchbnb.jsx
+++ BenchBnB/frontend/benchbnb.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// for testing
+
+import * as SessionApiUtil from './util/session_api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+    //for testing
+    window.login = SessionApiUtil.login
+    window.logout = SessionApiUtil.logout
+    window.signup = SessionApiUtil.signup
+
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
