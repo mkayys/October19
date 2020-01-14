af318b0bf2edfb9d0f3140cb4863e29e9dadec5a
Michelle Kim <x1michelleskim@gmail.com>
2020-01-13 16:17:50 -0800

    Create routes for users and tweets

--- MERNtwitter/app.js
+++ MERNtwitter/app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -14,5 +16,7 @@ app.get("/", (req, res) => res.send("hi World"));
 
 const port = process.env.PORT || 5000;
 
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
