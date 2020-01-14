c880eef3018da9903c148cbfad94bb42e4fa8ae9
Michelle Kim <x1michelleskim@gmail.com>
2020-01-13 16:03:14 -0800

    Sucessful connection with MongoDB and gitignore node_modules

--- MERNtwitter/app.js
+++ MERNtwitter/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => res.send("Hello World"));
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => res.send("hi World"));
 
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
